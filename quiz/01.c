29e581436d97ee298756ef5478f5cc97d6a78415
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-07-16 21:32:25 +0700

    Quiz01

---
+++ quiz/01.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+int main() {
+    int a = 17, b = 25, c = 50, d = 10;
+
+    //Before
+    printf("Before: a = %d, b = %d, c = %d, d = %d\n", a,b,c,d);
+    printf("-------\n");
+    //After
+
+    //Expression here
+    -++a;
+
+    printf("After: a = %d, b = %d, c = %d, d = %d", a,b,c,d);
+    return 0;
+}
