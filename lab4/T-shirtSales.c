276583123872afda43306367d24c9f5d6e4d5728
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-07-24 20:08:45 +0700

    Update lab04

--- lab4/T-shirtSales.c
+++ lab4/T-shirtSales.c
@@ -9,7 +9,7 @@ int main(){
     scanf("%f", &amount);
 
     float pro1 =  amount * shirt_p * (1 - (disc/100));
-    float pro2 = (amount - ceil((amount / 3))) * shirt_p;
+    float pro2 = (amount - floor((amount / 3))) * shirt_p;
 
     if (pro1 <= pro2) printf("Discount %.0f%%\n%.2f", disc, pro1);
     else printf("Buy 2 Get 1\n%.2f", pro2);
