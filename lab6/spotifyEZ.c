27a61aab5d99129afca0d82cdad3a08a364c0929
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-08-07 23:48:37 +0700

    Unfinish spotify

---
+++ lab6/spotifyEZ.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    long minute;
+    int second;
+    int n;
+
+    scanf("%ld.%d", &minute, &second);
+    scanf("%d", n);
+
+    char songName[n][100];
+    for (int i=0; i<n; i++) scanf(" Queue#%d <|> %[^<] <|> %d.%d", );
+}
