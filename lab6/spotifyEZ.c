98245b10bce052ab653d5e2c2d3102e5ec0b5e3f
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-08-08 12:56:40 +0700

    Spotify NotEZ

--- lab6/spotifyEZ.c
+++ lab6/spotifyEZ.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(){
-    long minute;
-    int second;
+    long minute_have;
+    int second_have;
     int n;
+    int inputM, inputS;
 
-    scanf("%ld.%d", &minute, &second);
-    scanf("%d", n);
+    scanf("%ld.%d", &minute_have, &second_have);
+    scanf("%d", &n);
 
-    char songName[n][100];
-    for (int i=0; i<n; i++) scanf(" Queue#%d <|> %[^<] <|> %d.%d", );
+    long totalTime = minute_have * 60 + second_have;
+    char songName[n][51];
+    int songDutation[n];
+
+    for (int i=0; i<n; i++){
+        scanf(" Queue#%*d <|> %[^<] <|> %d.%d", songName[i], &inputM, &inputS);
+        songDutation[i]= inputM * 60 + inputS;
+    } 
+    int ind=0;
+
+    while (1){
+        int dur = songDutation[ind];
+        if (totalTime < dur){
+            printf("Song Order: %d\n", ind+1);
+            printf("Sonf Name: %s\n", songName[ind]);
+            int percent = (100*totalTime) / dur;
+            if (percent == 0 && totalTime > 0) percent = 1;
+            printf("Song Process: %d%%", percent);
+            break;
+        }else if(totalTime == dur){
+            printf("Song Order: %d\n", ind + 1);
+            printf("Song Name: %s\n", songName[ind]);
+            printf("Song Process: Complete");
+            break;
+        }
+        totalTime -= dur;
+        ind = (ind + 1) % n;
+    }
+
+
+    
 }
