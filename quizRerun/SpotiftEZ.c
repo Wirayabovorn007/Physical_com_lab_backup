a91759c64dc42ef6cb008f55c3f54b6e0328412c
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-08-14 22:19:53 +0700

    Update spotify

--- quizRerun/SpotiftEZ.c
+++ quizRerun/SpotiftEZ.c
@@ -16,25 +16,23 @@ int main(){
         songLen[i] = min*60+sec;
         totalLenInQueue += songLen[i];
     }
-    int timeleft = totalUserSec%totalLenInQueue;
+    int timeleft = totalUserSec%totalLenInQueue; //เอาแค่เวลาที่เกินมาจากรอบสุดท้าย
     if (timeleft == 0) {
         printf("Song Order: %d\nSong Name: %s\nSong Process: Complete",queueInd[n-1], songs[n-1]);
         return 0;
     }
     for (int i=0; i< n; i++){
-        if (timeleft <= songLen[i]){
-            printf("Song Order: %d\nSong Name: %s\n",queueInd[i], songs[i]);
-            if (songLen[i] == timeleft){
-                printf("Song Process: Complete");
-                return 0;
-            }
-            else{
-                int percent = (timeleft * 100) / songLen[i];
-                if (percent ==0) percent=1;
-                if (percent >= 100) percent = 99;
-                printf("Song Process: %d%%", percent);
-                return 0;
-            }
+        if (timeleft < songLen[i]){
+            printf("Song Order: %d\nSong Name: %s\n", queueInd[i], songs[i]);
+            int percent = (timeleft * 100) / songLen[i];
+            if (percent == 0) percent++;
+            if (percent >= 100) percent = 99;
+            printf("Song Process: %d%%", percent);
+            return 0;
+        }
+        if (timeleft == songLen[i]){
+            printf("Song Order: %d\nSong Name: %s\nSong Process: Complete",queueInd[i], songs[i]);
+            return 0;
         }
         timeleft -= songLen[i];
     }
