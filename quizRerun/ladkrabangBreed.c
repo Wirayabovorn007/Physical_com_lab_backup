2ef7c175be2ebe2b44ded0a33d286cee6d32d769
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-08-14 18:29:42 +0700

    Almost finish Quiz rerun

--- quizRerun/ladkrabangBreed.c
+++ quizRerun/ladkrabangBreed.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+float getTankVal(char *name){
+	if (strcmp(name, "Nano") == 0) return 6.6;
+	if (strcmp(name, "Toh") == 0) return 5.5;
+	if (strcmp(name, "Mickey") == 0) return 5.5;
+}
 
 int main(){
-	float n =  ceil(0.13);
-	printf("%f\n", ceil(0.13));
-	printf("%f\n", ceil(n));
+	int n;
+	char name1[10], name2[10], name3[10];
+	float n1=0, n2=0,n3=0;
+	float n1use, n2use,n3use;
+
+	scanf("%d", &n);
+	scanf("%s %s %s", name1, name2, name3);
+	for (int i=0; i<n;i++) {
+		scanf("%f %f %f", &n1use, &n2use, &n3use);
+		n1+=n1use;
+		n2+=n2use;
+		n3+=n3use;
+	}
+	
+	int cal1 = ceil(n1 / getTankVal(name1));
+	int cal2 = ceil(n2 / getTankVal(name2));
+	int cal3 = ceil(n3 / getTankVal(name3));
+	printf("%s: %d refills\n%s: %d refills\n%s: %d refills", name1, cal1, name2,cal2, name3, cal3);
 }
--- quizRerun/whichWayBetter.c
+++ quizRerun/whichWayBetter.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
 int main(){
-	
+	float fuel_price;
+    int way[4];
+    float consumption_r[4];
+    float toll = 0;
+    float sumOfExpress = 0;
+    float sumRomkloa = 0;
+
+    scanf("%f", &fuel_price);
+    for (int i=0; i<4; i++) scanf("%d", &way[i]);
+    for (int i=0; i<4; i++) scanf("%f", &consumption_r[i]);
+    for (int i=0; i<4; i++){
+        int distance = way[i] == 0? 25:29;
+        float liters = (float)distance/consumption_r[i];
+        toll = way[i] == 0? 0:60;
+        float cost = (liters * fuel_price) + toll;
+        if (way[i] == 0) sumRomkloa += cost;
+        else sumOfExpress += cost;
+        printf("Day %d: fuel %.2f L, cost %.2f Baht\n", i+1,liters, cost);
+    }
+    int countRomKlaoW = 0, countExpressW = 0;
+    for (int i=0;i<4;i++) if (way[i] == 0)  countRomKlaoW++; else countExpressW++;
+    if (countExpressW == 0) countExpressW++;
+    if (countRomKlaoW == 0) countRomKlaoW++;
+    printf("Expressway: %.2f Baht\n", sumOfExpress / countExpressW);
+    printf("Romklao: %.2f Baht", sumRomkloa / countRomKlaoW);
 }
