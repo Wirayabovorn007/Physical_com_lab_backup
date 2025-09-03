0a8b79bdb3391497495d79f6056e4c1a368e564f
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2025-09-03 20:52:04 +0700

    Update

--- lab8/jumpingStr.c
+++ lab8/jumpingStr.c
@@ -8,9 +8,12 @@ int main(){
 
 	char *str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str) return 1;
-	scanf(" %[^\n]", str);
+	scanf(" %828720[^\n]", str);
 	char *p = str;
-	if (step == 0) return 0;
+	if (step == 0){
+		free(str);
+		return 0;
+	}
 	if (step > 0) for (int i=0; i<len; i+=step) printf("%c", *(p + i));
 	else if (step < 0) for (int i=len-1; i>=0; i+=step) printf("%c", *(p + i));
 	free(str);
